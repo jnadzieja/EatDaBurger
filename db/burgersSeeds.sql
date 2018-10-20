837ab8c38f468e92f2ee65be23e1f6eca6114488
jnadzieja <joshua.nadzieja@gmail.com>
2018-10-20 17:20:11 -0400

    initial push

---
+++ db/burgersSeeds.sql
@@ -0,0 +1,3 @@
+INSERT burgers_db.burgers (burger_name, devoured) VALUES ("Munch n' Crunch Burger", false);
+INSERT burgers_db.burgers (burger_name, devoured) VALUES ("Big as Texas Burger", false);
+INSERT burgers_db.burgers (burger_name, devoured) VALUES ("Spooky Burger", false);
